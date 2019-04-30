2ac72e902215bf576bc394c40317a0a5a185bfbe
Wes Price <wprice@hawaii.edu>
2019-04-30 09:57:31 -1000

    blank initializr project

---
+++ src/main/groovy/edu/hawaii/its/hack/HackApplication.groovy
@@ -0,0 +1,11 @@
+package edu.hawaii.its.hack
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class HackApplication {
+	static void main(String[] args) {
+		SpringApplication.run(HackApplication, args)
+	}
+}
---
+++ src/test/groovy/edu/hawaii/its/hack/HackApplicationTests.groovy
@@ -0,0 +1,16 @@
+package edu.hawaii.its.hack
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class HackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
