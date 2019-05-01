48c8d02718aaf57e5ec7cb16da0e1c24959366d7
Wes Price <wprice@hawaii.edu>
2019-05-01 08:57:08 -1000

    checkpoint

--- src/main/groovy/edu/hawaii/its/hack/HackApplication.groovy
+++ src/main/groovy/edu/hawaii/its/hack/HackApplication.groovy
@@ -5,6 +5,11 @@ import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.http.MediaType
+import org.springframework.http.RequestEntity
+import org.springframework.web.client.RestTemplate
 
 import edu.internet2.middleware.grouperClient.api.GcGetGroups
 import edu.internet2.middleware.grouperClient.ws.StemScope
@@ -14,6 +19,7 @@ import edu.internet2.middleware.grouperClient.ws.beans.WsGroup
 import edu.internet2.middleware.grouperClient.ws.beans.WsResultMeta
 import edu.internet2.middleware.grouperClient.ws.beans.WsStemLookup
 import edu.internet2.middleware.grouperClient.ws.beans.WsSubjectLookup
+import groovy.json.JsonOutput
 import groovy.util.logging.Slf4j
 
 @Slf4j
@@ -22,22 +28,104 @@ class HackApplication implements ApplicationRunner {
   @Value("\${test.admin.uhuuid}")
   String testAdminUhuuid
 
+  @Value("\${grouper.url}")
+  String grouperUrl
+
+  @Value("\${grouper.username}")
+  String grouperUsername
+
+  @Value("\${grouper.password}")
+  String grouperPassword
+
+  @Value("\${grouper.search.root}")
+  String grouperSearchRoot
+
   static void main(String[] args) {
     SpringApplication.run(HackApplication, args)
   }
 
   @Override
   void run(ApplicationArguments args) throws Exception {
-    thirdTry()
+    queryAllInSubtree()
+    querySeparateSubtrees()
+    queryAllInSubtree()
+    querySeparateSubtrees()
   }
 
-  void thirdTry() {
+  @Bean
+  RestTemplate grouperTemplate(RestTemplateBuilder builder) {
+    builder
+        .basicAuthentication(grouperUsername, grouperPassword)
+        .build()
+  }
+
+  static String stemQuery(String stemName, boolean recurse) {
+    // creating aliases for variables in the query
+    def nameKey = 'stemName'
+    def scopeKey = 'stemScope'
+    def placeholder = 'placeholder'
+
+    def stemEntry = [(nameKey):placeholder]
+    def requestMap = [
+        "subjectLookups": [
+            ["subjectId": "11733562"],
+        ],
+        "wsStemLookup": stemEntry,
+        (scopeKey): placeholder,
+        "enabled": "T",
+    ]
+    def queryMap = [
+        "WsRestGetGroupsRequest": requestMap
+    ]
+
+    stemEntry[nameKey] = stemName
+    requestMap[scopeKey] = recurse ? 'ALL_IN_SUBTREE' : 'ONE_LEVEL'
+    JsonOutput.toJson(queryMap)
+  }
+
+  RequestEntity<String> stemEntity(String stemName, boolean recurse) {
+    String queryJson = stemQuery(stemName, recurse)
+
+    URI subjectUri = new URL("${grouperUrl}/v2_2_200/subjects").toURI()
+    RequestEntity
+        .post(subjectUri)
+        .contentType(new MediaType('text', 'x-json'))
+        .body(queryJson)
+  }
+
+  void querySeparateSubtrees() throws Exception {
+    String rolesStem = 'hawaii.edu:custom:uhsystem:its:mis:forms-for-onbase:admin'
+    String formsStem = 'hawaii.edu:custom:uhsystem:its:mis:forms-for-onbase:forms'
+
+    RequestEntity<String> rolesEntity = stemEntity(rolesStem, false)
+    RequestEntity<String> formsEntity = stemEntity(formsStem, false)
+
+    RestTemplate t = grouperTemplate()
+    def rolesResult = t.exchange(rolesEntity, String)
+    def formsResult = t.exchange(formsEntity, String)
+    log.error "raw results: \n-----\n${rolesResult}\n-----\n${formsResult}\n-----\n"
+  }
+
+  // UNF: need to intentionally create some errors and then write code to handle (converting) them
+  // UNF: need to actively investigate pooling, especially for the two-query version
+
+  void queryAllInSubtree() throws Exception {
+    String allStem = 'hawaii.edu:custom:uhsystem:its:mis:forms-for-onbase'
+
+    RequestEntity<String> allEntity = stemEntity(allStem, true)
+
+    RestTemplate t = grouperTemplate()
+    def allResult = t.exchange(allEntity, String)
+    log.error "raw results: \n-----\n${allResult}\n-----\n"
+  }
+
+  void grouperClientQuery() {
     GcGetGroups gcGetGroups = new GcGetGroups()
         .addSubjectLookup(
             new WsSubjectLookup(testAdminUhuuid, null, null)
         )
         .assignWsStemLookup(
-            new WsStemLookup('hawaii.edu:custom:uhsystem:its:mis:forms-for-onbase', null)
+            new WsStemLookup(grouperSearchRoot, null)
         )
         .assignStemScope(StemScope.ALL_IN_SUBTREE)
         .assignEnabled(true)
@@ -57,29 +145,6 @@ class HackApplication implements ApplicationRunner {
       for (WsGroup group : result.getWsGroups()) {
         log.error "group: (${group.name}, ${group.typeOfGroup}, ${group.displayExtension})"
       }
-    }
-  }
-
-  void firstTry() {
-    try {
-      log.error("---------------- pre-grouper  ----------------------")
-
-      WsGetGroupsResults wsGetGroupsResults = new GcGetGroups()
-          .addSubjectIdentifier(testAdminUhuuid)
-          .assignStemScope(StemScope.ONE_LEVEL)
-          .assignWsStemLookup(
-              new WsStemLookup('hawaii.edu:custom:uhsystem:its:mis:forms-for-onbase', null)
-          )
-          .assignIncludeSubjectDetail(Boolean.FALSE)
-          .execute()
-      log.error("groups: ${wsGetGroupsResults}")
-      log.error("---------------- post-grouper ----------------------")
-    }
-
-    catch (
-        Exception e
-        ) {
-      log.error("grouper exception", e)
     }
   }
 }
---
+++ src/main/resources/logback.groovy
@@ -0,0 +1,31 @@
+// uncomment next line to debug logback itself
+// statusListener(OnConsoleStatusListener)
+
+String logDir = 'target/logs'
+String logName = 'hack'
+
+String layoutPattern = '%date %5level %property{PID} --- [%15.15thread] %logger : %m%n'
+
+appender('LOGFILE', FileAppender) {
+  encoder(PatternLayoutEncoder) { pattern = layoutPattern }
+
+  file = "${logDir}/${logName}.log"
+}
+
+appender('CONSOLE', ConsoleAppender) {
+  encoder(PatternLayoutEncoder) { pattern = layoutPattern }
+}
+
+// configure appenders
+//   everything defaults to INFO
+root(INFO, ['CONSOLE', 'LOGFILE'])
+
+// override log levels
+logger('edu.hawaii', DEBUG)
+logger('org.springframework.security', DEBUG)
+
+// enable debugging for web components
+logger('web', DEBUG)
+
+// disable tomcat noise
+logger('org.apache.catalina.core.AprLifecycleListener', WARN)
