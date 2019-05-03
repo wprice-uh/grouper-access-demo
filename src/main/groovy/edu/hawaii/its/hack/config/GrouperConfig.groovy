c46e816883a04929b018217fdc4aa5d0d0814848
Wes Price <wprice@hawaii.edu>
2019-05-03 13:32:08 -1000

    added BufferingClientHttpRequestFactory

--- src/main/groovy/edu/hawaii/its/hack/config/GrouperConfig.groovy
+++ src/main/groovy/edu/hawaii/its/hack/config/GrouperConfig.groovy
@@ -8,6 +8,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType
+import org.springframework.http.client.BufferingClientHttpRequestFactory
 import org.springframework.http.client.ClientHttpRequestFactory
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
@@ -32,6 +33,9 @@ class GrouperConfig {
   @Value("\${grouper.password}")
   String grouperPassword
 
+  /**
+   * @return RestTemplate configured to invoke grouper
+   */
   @Bean
   RestTemplate grouperTemplate(RestTemplateBuilder builder) {
     log.debug 'instantiating grouperTemplate'
@@ -44,23 +48,45 @@ class GrouperConfig {
         .build()
   }
 
+  /**
+   * @return messageConverter with support for the non-standard MediaType
+   * returned by grouper
+   */
   @Bean
   MappingJackson2HttpMessageConverter grouperConverter() {
+    log.debug 'instantiating grouperConverter'
+
     new MappingJackson2HttpMessageConverter().tap {
       supportedMediaTypes = [new MediaType('text', 'x-json', StandardCharsets.UTF_8)]
       objectMapper = grouperObjectMapper()
     }
   }
 
+  /**
+   * @return ObjectMapper configured for processing grouper responses
+   */
   @Bean
   ObjectMapper grouperObjectMapper() {
+    log.debug 'instantiating grouperObjectMapper'
+
     new ObjectMapper().tap {
+      // ignore unrecognized properties, so that my json model classes need
+      // only contain values I'm actively interested in using
       configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false)
+
+      // unwrap the top-level object, to simplify accessing subcomponents
       configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true)
+
+      // allow single-value arrays to be flattened into that single value,
+      // to simplify accessing subcomponents
       configure(DeserializationFeature.UNWRAP_SINGLE_VALUE_ARRAYS, true)
     }
   }
 
+  /**
+   * @return ResponseErrorHandler to prevent some 500 errors from throwing
+   * uselessly-generic exceptions
+   */
   @Bean
   ResponseErrorHandler grouperErrorHandler() {
     log.debug 'instantiating grouperErrorHandler'
@@ -68,8 +94,10 @@ class GrouperConfig {
     new GrouperErrorHandler(grouperObjectMapper())
   }
 
-  // had to create a supplier to wrap the connectionPoolingFactory because
-  // the compiler was getting confused
+  /**
+   * @return ClientHttpRequestFactory required to plug grouperRequestFactory
+   * into the RestTemplate infrastructure
+   */
   @Bean
   Supplier<ClientHttpRequestFactory> grouperRequestFactorySupplier() {
     log.debug 'instantiating grouperRequestFactorySupplier'
@@ -77,15 +105,24 @@ class GrouperConfig {
     new Supplier() {
       @Override
       ClientHttpRequestFactory get() {
-        grouperConnectionPoolingFactory()
+        grouperRequestFactory()
       }
     }
   }
 
+  /**
+   * Replaces the default factory with one which uses apache's HttpComponents,
+   * to gain more configurability. Wraps the configured factory with a wrapper
+   * which supports reading the body multiple times, since I need that to
+   * parse responses having 500 status (see {@link GrouperErrorHandler})
+   * without breaking further response-processing
+   */
   @Bean
-  HttpComponentsClientHttpRequestFactory grouperConnectionPoolingFactory() {
-    log.debug 'instantiating grouperConnectionPoolingFactory'
+  ClientHttpRequestFactory grouperRequestFactory() {
+    log.debug 'instantiating grouperRequestFactory'
 
-    new HttpComponentsClientHttpRequestFactory()
+    new BufferingClientHttpRequestFactory(
+        new HttpComponentsClientHttpRequestFactory()
+    )
   }
 }
